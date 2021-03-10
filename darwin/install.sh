f433b1316e06a1a77967176c1fa61f30461cb188
Dane Harrigan <94443+daneharrigan@users.noreply.github.com>
2021-03-09 16:47:37 -0800

    WIP

---
+++ darwin/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#echo "Installing Xcode"
+#xcode-select --install 2> /dev/null
+#
+#echo "Installing Homebrew"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#
+#echo "Installing Homebrew casks"
+#brew bundle install
+#
+#echo "Installing ansible"
+#pip3 install ansible
+
+chsh -s /usr/local/bin/zsh
+PATH=$(pip3 show ansible | grep Location | cut -d " " -f 2):$PATH
+ansible-playbook -K ../files/shell.yaml
